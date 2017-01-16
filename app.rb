274f2246402814d3619d8aa4400c51edf86fabb5
Joe Marriott <marriott90@gmail.com>
2017-01-16 17:30:51 +0000

    returning HTML with sinatra

--- app.rb
+++ app.rb
@@ -11,3 +11,12 @@ end
 get '/shotgun' do
   "Shotgun is working! I'm proof of that!"
 end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src ='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
+
+set :session_secret, 'super secret'
