2951f4f04edd3dac1f7dc8cdec1689a1581b48c7
Joe Marriott <marriott90@gmail.com>
2017-01-17 15:40:15 +0000

    sinatra: posted parameters

--- app.rb
+++ app.rb
@@ -17,10 +17,14 @@ get '/random_cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
 
 
